0598122be8a6e45890f3cbc92039905ea3ff597f
Domaman202 <vip.domaman@mail.ru>
2025-12-06 09:01:45 +0700

    =)

--- src/main/kotlin/ru/pht/sprout/module/repo/IRepository.kt
+++ src/main/kotlin/ru/pht/sprout/module/repo/IRepository.kt
@@ -1,5 +1,6 @@
 package ru.pht.sprout.module.repo
 
+import io.github.z4kn4fein.semver.constraints.Constraint
 import java.io.IOException
 
 /**
@@ -13,7 +14,7 @@ interface IRepository {
      * @return Список модулей соответствующих имени.
      */
     @Throws(IOException::class)
-    fun find(name: String): List<IDownloadable>
+    fun find(name: String, version: Constraint): List<IDownloadable>
 
     /**
      * Асинхронный поиск модулей.
@@ -22,5 +23,5 @@ interface IRepository {
      * @return Список модулей соответствующих имени.
      */
     @Throws(IOException::class)
-    suspend fun findAsync(name: String): List<IDownloadable>
+    suspend fun findAsync(name: String, version: Constraint): List<IDownloadable>
 }
